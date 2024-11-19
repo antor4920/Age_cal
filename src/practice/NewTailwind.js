74a5643407f28770d8659dabc26c4879e2dcc3d0
antor4920 <antor4920@gmail.com>
2024-11-19 15:56:58 +0600

    9

--- src/practice/NewTailwind.js
+++ src/practice/NewTailwind.js
@@ -1,28 +1,32 @@
 import React from "react";
+import Section1 from "./Section1";
 
 const NewTailwind = () => {
   return (
-    <nav className="sm:flex">
-      <ul className="bg-slate-400 flex gap-16 py-4 justify-evenly   ">
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">Shopping</a>
-        </li>
-        <li>
-          <a href="">products</a>
-        </li>
-        <li>
-          <a href="">About</a>
-        </li>
-      </ul>
-      <div className=" bg-slate-500 text-center sm:h-auto">
-        <button className="h-[40px] w-[200px] bg-orange-400  rounded-xl ">
-          Contact
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="sm:flex bg-green-400 sm:justify-around sm:items-center transition-all duration-500">
+        <ul className=" flex gap-12 py-4 justify-evenly  sm:gap-10  transition-all duration-500 ">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Shopping</a>
+          </li>
+          <li>
+            <a href="">products</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+        </ul>
+        <div className=" bg-green-400 text-center sm:w-auto sm:h-auto sm:bg-green-400 transition-all duration-500  ">
+          <button className="h-[40px] w-[200px] bg-green-800  rounded-xl  mb-2 sm:mb-0">
+            Contact
+          </button>
+        </div>
+      </nav>
+      <Section1 />
+    </>
   );
 };
 
---
+++ src/practice/Section1.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Section1 = () => {
+  return (
+    <div className=" flex flex-col items-center sm:flex-row sm:justify-evenly transition-all duration-500 ">
+      <div className="w-[350px] h-[350px] bg bg-green-200 rounded-full mt-10 sm:w-[250px] sm:h-[250px] md:w-[350px] md:h-[350px] transition-all duration-500"></div>
+      <div className=" text-center mt-16 sm:w-[350px] md:w-[450px] transition-all duration-100 ">
+        <h3 className="font-bold text-xl text-green-400">Hello, I'm</h3>{" "}
+        <h1 className="font-extrabold text-2xl text-green-600">
+          Jahidul hasna Antor
+        </h1>
+        <p className="text-green-900">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, amet?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Section1;
