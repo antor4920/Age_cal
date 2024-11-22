79629076c0f0d60440dbf5b130374bdaf2c45efa
antor4920 <antor4920@gmail.com>
2024-11-22 23:54:50 +0600

    m

--- src/practice/Section1.js
+++ src/practice/Section1.js
@@ -6,7 +6,7 @@ const Section1 = () => {
       <div className="w-[350px] h-[350px] bg bg-green-200 rounded-full mt-10 sm:w-[250px] sm:h-[250px] md:w-[350px] md:h-[350px] transition-all duration-500"></div>
       <div className=" text-center mt-16 sm:w-[350px] md:w-[450px] transition-all duration-100 ">
         <h3 className="font-bold text-xl text-green-400">Hello, I'm</h3>{" "}
-        <h1 className="font-extrabold text-2xl text-green-600">
+        <h1 className="font-extrabold text-2xl text-green-800">
           Jahidul hasna Antor
         </h1>
         <p className="text-green-800">
